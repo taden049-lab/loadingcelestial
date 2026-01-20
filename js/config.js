5306acc39389262aa750b85e1ab610e72d6324c7
taden049-lab <taden049@gmail.com>
2026-01-20 15:30:41 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "";
+Config.title = "ТЕСТО ТЕСТО";
 
 /**
  * Enable map text in the top left corner of the screen?
