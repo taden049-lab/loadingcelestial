9a02d08ef9f1bdacc19602127f042f6168e86fb5
taden049-lab <taden049@gmail.com>
2026-01-20 15:31:48 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "ТЕСТО ТЕСТО";
+Config.title = "";
 
 /**
  * Enable map text in the top left corner of the screen?
@@ -27,8 +27,8 @@ Config.enableAnnouncements = true;
  * only works if enableAnnouncements = true
  */
 Config.announceMessages = [
-  "Enjoy your stay!",
-  "This loading screen is made by Exsite."
+  "Добро Пожаловать, не забудь поставить RP Nick!",
+  "Просим вас соблюдать правила и не нарушать их."
 ];
 
 /**
